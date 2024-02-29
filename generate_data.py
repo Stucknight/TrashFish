b5d13bf44399f224f9c0db69b954d31a7ee9710a
Stucknight <chengweifan2020@gmail.com>
2024-03-01 00:44:17 +0800

    seperated generation and training

---
+++ generate_data.py
@@ -0,0 +1,48 @@
+import numpy as np
+import chess
+import random
+import chess.engine
+
+
+def stockfish(board, depth):
+    with chess.engine.SimpleEngine.popen_uci('stockfish/stockfish-windows-x86-64-avx2.exe') as sf:
+        result = sf.analyse(board, chess.engine.Limit(depth=depth))
+        return result['score'].white().score()
+
+
+def create_board(depth):
+    board = chess.Board()
+    board_depth = random.randint(0, depth)
+
+    for _ in range(board_depth):
+        move = random.choice(list(board.legal_moves))
+        board.push(move)
+        if board.is_game_over():
+            return None
+    return board
+
+
+def convert_matrix(board):
+    board3d = np.zeros((13, 8, 8), dtype=np.int8)
+    for square, piece in board.piece_map().items():
+        color = piece.color
+        index = np.unravel_index(square, (8, 8))
+        p_index = piece.piece_type - 1 if color == chess.WHITE else piece.piece_type + 5
+        board3d[p_index][7 - index[0]][index[1]] = 1
+    return board3d
+
+
+def generate_data(num_samples, depth, filename):
+    X, y = [], []
+    for _ in range(num_samples):
+        board = create_board(depth)
+        if board is not None:
+            X.append(convert_matrix(board))
+            y.append(stockfish(board, 20))
+    X = np.array(X)
+    y = np.array(y)
+    print(y)
+    np.savez(filename, X=X, y=y)
+
+
+generate_data(100, 20, "dataset.npz")
--- training.py
+++ training.py
@@ -5,34 +5,6 @@ import chess.engine
 from tensorflow.keras import models, layers, optimizers
 
 
-def stockfish(board, depth):
-    with chess.engine.SimpleEngine.popen_uci('stockfish/stockfish-windows-x86-64-avx2.exe') as sf:
-        result = sf.analyse(board, chess.engine.Limit(depth=depth))
-        return result['score'].white().score()
-
-
-def create_board(depth):
-    board = chess.Board()
-    board_depth = random.randint(0, depth)
-
-    for _ in range(board_depth):
-        move = random.choice(list(board.legal_moves))
-        board.push(move)
-        if board.is_game_over():
-            return None
-    return board
-
-
-def convert_matrix(board):
-    board3d = np.zeros((13, 8, 8), dtype=np.int8)
-    for square, piece in board.piece_map().items():
-        color = piece.color
-        index = np.unravel_index(square, (8, 8))
-        p_index = piece.piece_type - 1 if color == chess.WHITE else piece.piece_type + 5
-        board3d[p_index][7 - index[0]][index[1]] = 1
-    return board3d
-
-
 def train_model(size):
     model = models.Sequential([
         layers.Conv2D(size, kernel_size=4, padding='same', activation='relu', input_shape=(13, 8, 8)),
@@ -46,22 +18,17 @@ def train_model(size):
     return model
 
 
-def generate_data(num_samples, depth):
-    X, y = [], []
-    for _ in range(num_samples):
-        board = create_board(depth)
-        if board is not None:
-            X.append(convert_matrix(board))
-            y.append(stockfish(board, 10))
-    return np.array(X), np.array(y)
+def load_dataset(filename):
+    data = np.load(filename, allow_pickle=True)
+    return data["X"], data["y"]
 
 
-def train(size, num_samples, epochs):
+def train(size, filename, epochs):
     model = train_model(size)
-    X, y = generate_data(num_samples, 20)
+    X, y = load_dataset("dataset.npz")
     model.fit(X, y, epochs=epochs, batch_size=32, verbose=1)
     return model
 
 
-model = train(size=32, num_samples=100, epochs=100)
+model = train(size=32, filename="dataset.npz", epochs=100)
 model.save('model.keras')
